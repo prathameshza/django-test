692b7b6382c7e5fabc03de046ecfc0b7ab4640b8
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-05 21:20:19 +0530

    Update settings.py

--- hover/settings.py
+++ hover/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ROOT_URLCONF = "hover.urls"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
