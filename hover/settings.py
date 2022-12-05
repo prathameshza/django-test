a7fcb16b6dd5c276bbac941947aa15732a162c9d
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-05 20:32:12 +0530

    Update settings.py

--- hover/settings.py
+++ hover/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 
 
 # Default primary key field type
@@ -124,9 +124,9 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-#STATICFILES_DIRS=[
-#    'static'
-#]
+STATICFILES_DIRS=[
+   'static'
+]
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_URL = 'static/'
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+# STATIC_URL = 'static/'
