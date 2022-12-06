03196496bfdbeec5d4e8ead7287ab3ce52be3651
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-06 19:22:18 +0530

    Update settings.py

--- hover/settings.py
+++ hover/settings.py
@@ -127,17 +127,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+#STATICFILES_DIRS=[
+#    'static',
+#    ]
 
-# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-# STATIC_URL = 'static/'
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
-   'static'
-]
-
-# Add these new lines
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
