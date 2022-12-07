70a9c0228c3f0734b6f0047cff3decb8df67d2a1
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-07 19:36:57 +0530

    Update settings.py

--- hover/settings.py
+++ hover/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "hover.urls"
 
 
 # Quick-start development settings - unsuitable for production
