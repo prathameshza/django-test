eaea7482bb6adbeb81d56728635863940189a2cd
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-12-08 13:39:24 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='home')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
